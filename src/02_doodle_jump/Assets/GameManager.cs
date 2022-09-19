c89422d40974073d2839c0ab91738c80e8b2423e
Noé Landré <noe.landre@laposte.net>
2022-09-19 15:34:02 +0200

    feat: workshop 2 initial code (without ANY comments)

---
+++ src/02_doodle_jump/Assets/GameManager.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public GameObject platformPrefab;
+
+    public int platformCount = 20;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Vector3 spawnPosition = new Vector3();
+
+        for(int i = 0; i < platformCount; i++)
+        {
+            spawnPosition.y += Random.Range(0.25f, 1.5f);
+            spawnPosition.x = Random.Range(-2.5f, 2.5f);
+
+            Instantiate(platformPrefab, spawnPosition, Quaternion.identity);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
