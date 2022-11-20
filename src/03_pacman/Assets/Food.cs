030e94dd4a47dc094e0ac40de02278b3cbc362df
Noé Landré <noe.landre@laposte.net>
2022-11-20 18:29:56 +0100

    feat: add win

--- src/03_pacman/Assets/Food.cs
+++ src/03_pacman/Assets/Food.cs
@@ -12,14 +12,22 @@ public class Food : MonoBehaviour
     {
         if (col.CompareTag("Player")) {
             Vector3 playerPos = col.transform.position;
-            int[] direction = col.GetComponent<PacMan>().direction;
+            PacMan playerScript = col.GetComponent<PacMan>();
+            
+            int[] direction = playerScript.direction;
             if (direction[0] != 0) {
                 float deltaX = direction[0] == -1 ? -1 : 0;
                 GetComponent<Tilemap>().SetTile(new Vector3Int((int) Math.Round(playerPos.x + deltaX), (int) Math.Floor(playerPos.y)), null);
             } else {
                 float deltaY = direction[1] == -1 ? -1 : 0;
-                GetComponent<Tilemap>().SetTile(new Vector3Int((int) Math.Floor(playerPos.x), (int) Math.Round(playerPos.y + deltaY)), null);
+                GetComponent<Tilemap>().SetTile(new Vector3Int((int) Math.Floor(playerPos.x), (int) Math.Round(playerPos.y + deltaY)), null);   
             }
+
+            playerScript.addScore(10);
         }
+    }
+
+    public int GetFoodCount() {
+        return GetComponent<Tilemap>().GetUsedTilesCount();
     }
 }
