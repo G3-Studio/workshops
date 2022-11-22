a3af42578ccfafbe51ab7c09241481bcdd41fbd2
Noé Landré <noe.landre@laposte.net>
2022-11-22 01:19:30 +0100

    feat: remake the project with new map

--- src/03_pacman/Assets/Food.cs
+++ src/03_pacman/Assets/Food.cs
@@ -14,14 +14,8 @@ public class Food : MonoBehaviour
             Vector3 playerPos = col.transform.position;
             PacMan playerScript = col.GetComponent<PacMan>();
             
-            int[] direction = playerScript.direction;
-            if (direction[0] != 0) {
-                float deltaX = direction[0] == -1 ? -1 : 0;
-                GetComponent<Tilemap>().SetTile(new Vector3Int((int) Math.Round(playerPos.x + deltaX), (int) Math.Floor(playerPos.y)), null);
-            } else {
-                float deltaY = direction[1] == -1 ? -1 : 0;
-                GetComponent<Tilemap>().SetTile(new Vector3Int((int) Math.Floor(playerPos.x), (int) Math.Round(playerPos.y + deltaY)), null);   
-            }
+            var tilePos = GetComponent<Tilemap>().WorldToCell(col.gameObject.transform.position);
+            GetComponent<Tilemap>().SetTile(tilePos, null);
 
             playerScript.addScore(10);
         }
