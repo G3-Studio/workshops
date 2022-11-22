91f746da5aae71e34921d47f4281c7def52cdc9a
Noé Landré <noe.landre@laposte.net>
2022-11-22 01:19:30 +0100

    feat: remake the project with new map

--- src/03_pacman/Assets/Scripts/PacMan.cs
+++ src/03_pacman/Assets/Scripts/PacMan.cs
@@ -31,10 +31,10 @@ public class PacMan : MonoBehaviour
         rb.velocity = new Vector3(speed * direction[0], speed * direction[1], 0);
         Vector3 position = transform.position;
         if (direction[0] != 0) {
-            transform.position = new Vector3(position.x, (float) Math.Round(position.y + 0.5) - 0.5f);
+            // transform.position = new Vector3(position.x, (float) Math.Round(position.y + 0.5) - 0.5f);
             transform.rotation = Quaternion.Euler(0, 0, (direction[0] - 1) * 90);
         } else {
-            transform.position = new Vector3((float) Math.Round(position.x + 0.5) - 0.5f, position.y);
+            // transform.position = new Vector3((float) Math.Round(position.x + 0.5) - 0.5f, position.y);
             transform.rotation = Quaternion.Euler(0, 0, direction[1] * 90);
         }
     }
