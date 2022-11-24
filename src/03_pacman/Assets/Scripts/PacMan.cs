d29be6a7e55c360200399666a997e3558c149e3c
Teddy Roncin <teddy.roncin35@gmail.com>
2022-11-24 14:20:09 +0100

    fixed collision boxes + now impossible to go in an impossible direction

--- src/03_pacman/Assets/Scripts/PacMan.cs
+++ src/03_pacman/Assets/Scripts/PacMan.cs
@@ -1,11 +1,14 @@
 using System;
 using UnityEngine;
+using UnityEngine.Tilemaps;
+using UnityEngine.UIElements;
 
 public class PacMan : MonoBehaviour
 {
 
     [SerializeField] private float speed;
     [SerializeField] public Grid grid;
+    [SerializeField] private Tilemap walls;
     private int[] direction = { 1, 0 };
     private Rigidbody2D rb;
 
@@ -18,13 +21,18 @@ public class PacMan : MonoBehaviour
 
     private void Update()
     {
-        float horizontal = Input.GetAxis("Horizontal");
-        float vertical = Input.GetAxis("Vertical");
-
-        if (horizontal != 0) {
-            direction = new[] { Math.Sign(horizontal), 0 };
-        } else if (vertical != 0) {
-            direction = new[] { 0, Math.Sign(vertical)};
+        int horizontal = Math.Sign(Input.GetAxis("Horizontal"));
+        int vertical = Math.Sign(Input.GetAxis("Vertical"));
+        Vector3Int position = GetPosition();
+        Debug.Log("" + horizontal + "  " + walls.GetTile(new Vector3Int(position.x + horizontal, position.y)) + "   " + position);
+        RaycastHit hitInfo;
+        Debug.Log(grid.WorldToCell(Camera.main.ScreenToWorldPoint(Input.mousePosition)));
+        //Debug.Log(Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hitInfo, Mathf.Infinity));
+        //Debug.Log(hitInfo.point);
+        if (horizontal != 0 && walls.GetTile(new Vector3Int(position.x + horizontal, position.y)) == null) {
+            direction = new int[] { horizontal, 0 };
+        } else if (vertical != 0 && walls.GetTile(new Vector3Int(position.x, position.y + vertical)) == null) {
+            direction = new int[] { 0, vertical };
         }
     }
 
